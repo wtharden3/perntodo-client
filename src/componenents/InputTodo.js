028e9c794d36901edc7e2b816769209e59c4ea14
Whitney Harden <wtharden320@gmail.com>
2022-02-25 09:24:36 -0500

    added functionality to InputTodo

--- src/componenents/InputTodo.js
+++ src/componenents/InputTodo.js
@@ -1,13 +1,39 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const InputTodo = () => {
+
+  const [description, setDescription] = useState("");
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = {description};
+      const response = fetch("http://localhost:4040/createtodo", {
+        method: "POST",
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify(body)
+      });
+
+      console.log(response);
+
+    } catch (err){
+      console.error(`[CLIENT ERROR] ${err.message}`);
+    }
+  }
+
   return(
     <Fragment>
       <h1 className="text-center mt-5">
         PERN Todo List
       </h1>
-      <form className="d-flex mt-5">
-        <input className="form-control" type="text" name="description"/>
+      <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+        <input 
+          className="form-control" 
+          type="text" 
+          name="description" 
+          value={description}
+          onChange={e => setDescription(e.target.value)}
+        />
         <button className="btn btn-info">Add</button>
       </form>
     </Fragment>
