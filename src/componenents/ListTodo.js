d6d0c0500d0c779cc5cb777d4b4f3b7a1192e438
Whitney Harden <wtharden320@gmail.com>
2022-02-26 10:51:47 -0500

    corrected count

--- src/componenents/ListTodo.js
+++ src/componenents/ListTodo.js
@@ -1,8 +1,10 @@
-import React, { Fragment, useState} from "react";
+import React, { Fragment, useState, useEffect } from "react";
 
 const ListTodo = () => {
   const [todos, setTodos] = useState(["todo 1","todo 2","todo 3"]);
   const [todoNum, setTodoNum] = useState(1);
+
+  // useEffect()
 
   return(
     <div className="mt-5">
@@ -10,15 +12,15 @@ const ListTodo = () => {
         <thead>
           <tr>
             <th scope="col">#</th>
-            <th scope="col">Description</th>
+            <th scope="col">Todo</th>
           </tr>
         </thead>
         <tbody>
-          {todos.map(todo => {
+          {todos.map((todo, index) => {
             return(
               <Fragment>
                 <tr>
-                  <th scope="row">{todoNum}</th>
+                  <th scope="row">{index+1}</th>
                   <td>{todo}</td>
                 </tr>
               </Fragment>
