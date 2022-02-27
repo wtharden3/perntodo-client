5ac2c3e422b3ad7b77b4c124a4c49aff7eddc23b
Whitney Harden <wtharden320@gmail.com>
2022-02-27 10:31:20 -0500

    created delete functionality

--- src/componenents/ListTodo.js
+++ src/componenents/ListTodo.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 const ListTodo = () => {
   const [todos, setTodos] = useState(["todo 1","todo 2","todo 3"]);
+  const [deletedTodo, setDeletedTodo] = useState(0);
   //I want to pass down InputTodos state so that I can make it a dependency
   //for the useEffect that is running fetch function as an argument.
   // useEffect() //used this for fetch
@@ -11,7 +12,9 @@ const ListTodo = () => {
         headers: { "Content-Type": "application/json"},
       })
       .then(res => res.json())
-      .then(data => setTodos(data))
+      .then(data => {
+        setTodos(data);
+      })
     }
     // converted the above function to trycatch using asyc await and storing await fetch in a function that should be returned...
   // const fetchTodosAsync = async () => {
@@ -28,7 +31,18 @@ const ListTodo = () => {
   //   }
   // }
 
-  useEffect(() => fetchTodos(), []) //will only run when initialized not when state changes
+  const handleDelete = (id) => {
+    console.log(`todos: ${id}`);
+    fetch(`http://localhost:4040/todo/${id}`, {
+      method: "DELETE",
+      headers: {"Content-Type": "application/json"}
+    }).then(res => {
+      console.log(res);
+      setDeletedTodo(id);
+    })
+  }
+
+  useEffect(() => fetchTodos(), [deletedTodo]) //will only run when initialized not when state changes
   // useEffect(() => fetchTodosAsync(), [])
   return(
     <div className="mt-5">
@@ -48,7 +62,7 @@ const ListTodo = () => {
                 <th scope="row">{index+1}</th>
                 <td >{todo.description}</td>
                 <td><button type="button" className="btn btn-outline-info">Edit</button></td>
-                <td><button type="button" className="btn btn-danger">Delete</button></td>
+                <td><button type="button" className="btn btn-danger" onClick={e => handleDelete(todo.todo_id)}>Delete</button></td>
               </tr>
             )   
           })}
