87d5de4ce7b548e33771fe3fa2f75151b93959e5
Whitney Harden <wtharden320@gmail.com>
2022-02-27 09:32:08 -0500

    added asyc await fetch function in comments for alt

--- src/componenents/ListTodo.js
+++ src/componenents/ListTodo.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 
 const ListTodo = () => {
   const [todos, setTodos] = useState(["todo 1","todo 2","todo 3"]);
+  //I want to pass down InputTodos state so that I can make it a dependency
+  //for the useEffect that is running fetch function as an argument.
   // useEffect() //used this for fetch
   const fetchTodos = () => {
     fetch("http://localhost:4040/alltodos", {
@@ -11,10 +13,23 @@ const ListTodo = () => {
       .then(res => res.json())
       .then(data => setTodos(data))
     }
-    // convert the above function to trycatch using asyc await and storing await fetch in a function that should be returned...
-    
-  useEffect(() => fetchTodos(), [])
+    // converted the above function to trycatch using asyc await and storing await fetch in a function that should be returned...
+  // const fetchTodosAsync = async () => {
+  //   try {
+  //     const response = await fetch("http://localhost:4040/alltodos", {
+  //         method: "GET",
+  //         headers: { "Content-Type": "application/json"}
+  //       });
+  //     const fetchJson = await response.json();
+  //     console.log(`fetchJson: ${fetchJson}`)
+  //     setTodos(fetchJson);
+  //   } catch (err) {
+  //     console.error(`[ERROR] ${err.message}`);
+  //   }
+  // }
 
+  useEffect(() => fetchTodos(), []) //will only run when initialized not when state changes
+  // useEffect(() => fetchTodosAsync(), [])
   return(
     <div className="mt-5">
       <table className="table">
@@ -22,6 +37,8 @@ const ListTodo = () => {
           <tr>
             <th scope="col">#</th>
             <th scope="col">Todo</th>
+            <th className="text-info" scope="col">Edit</th>
+            <th className="text-danger" scope="">Delete</th>
           </tr>
         </thead>
         <tbody>
